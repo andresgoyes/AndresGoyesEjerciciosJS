85e71c8c1792a9041c874a6fb12179886b03e1d6
andresgoyes <andres.goyes@correounivalle.edu.co>
2024-05-01 09:51:10 -0500

    ejercicios bucles hasta el 10

--- Ejercicios Bucles/js_bucles_exercises.js
+++ Ejercicios Bucles/js_bucles_exercises.js
@@ -30,11 +30,83 @@ while (intento_ej3 !== numSec_ej3) {
     intento_ej3 = parseInt(prompt('Ingresa un numero entre 1 y 100: '));
     acumIntentos_ej3++;
 
-    if (intento_ej3 < numSec_ej3){
+    if (intento_ej3 < numSec_ej3) {
         alert("El numero a adivinar es mayor");
-    } else if(intento_ej3 > numSec_ej3){
+    } else if (intento_ej3 > numSec_ej3) {
         alert("El numero a adivinar es menor");
     } else {
-        alert("Felicidades, adivinaste en: "+acumIntentos_ej3+" intentos");
+        alert("Felicidades, adivinaste en: " + acumIntentos_ej3 + " intentos");
     }
 }
+
+alert("Ejercicio 5");
+
+let num_ej5 = parseInt(prompt("Por favor, ingresa un numero: "));
+let div_ej5 = "Divisores de " + num_ej5 + ": ";
+
+for (let i = 1; i <= num_ej5; i++) {
+    if (num_ej5 % i == 0) {
+        div_ej5 += i + " ";
+    }
+
+} alert(div_ej5);
+
+alert("Ejercicio 6");
+
+let array_ej6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < array_ej6.length; i++) {
+    console.log("Elemento en la posición " + i + ": " + array_ej6[i]);
+}
+
+alert("Ejercicio 7");
+
+let array_ej7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < array_ej7.length; i++) {
+    console.log("Elemento en la posición " + i + ": " + array_ej7[i] * 2);
+}
+
+alert("Ejercicio 8");
+
+let familia_ej8 = [
+    { nombre: "Andres", edad: 38, relacion: "padre", hobby: "trabajar" },
+    { nombre: "Carolina", edad: 27, relacion: "madre", hobby: "cocinar" },
+    { nombre: "Samuel", edad: 17, relacion: "hijo mayor", hobby: "tocar guitarra" },
+    { nombre: "Santiago", edad: 5, relacion: "hijo menor", hobby: "jugar" },
+    { nombre: "Hector", edad: 28, relacion: "abuela", hobby: "estudiar" }
+];
+
+for (let i = 0; i < familia_ej8.length; i++) {
+    console.log(`Hola, soy ${familia_ej8[i].nombre}, tengo ${familia_ej8[i].edad} años, soy el/la ${familia_ej8[i].relacion} y me gusta ${familia_ej8[i].hobby}`);
+}
+
+alert("Ejercicio 9");
+
+let array_ej9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < array_ej9.length; i++) {
+    if (array_ej9[i] % 2 !== 0) {
+        console.log(array_ej9[i]);
+    }
+}
+
+alert("Ejercicio 10");
+
+let acuPar = 0;
+let acuImpar = 0;
+
+let num_ej10 = parseInt(prompt("Por favor, ingresa un numero: "));
+
+while (num_ej10 !== 0) {
+
+    if (num_ej10 % 2 == 0) {
+        acuPar += num_ej10;
+    } else {
+        acuImpar += num_ej10;
+    }    
+    num_ej10 = parseInt(prompt("Ingrese otro numero para ir sumando, escriba 0 para terminar:"));
+}
+
+console.log(`La suma de los números pares es: ${acuPar}`);
+console.log(`La suma de los números impares es: ${acuImpar}`);
